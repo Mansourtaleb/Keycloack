11cc366dcb75c0ac3b3791bd37f7ecbd76f48ffd
MansourTA <mohamedmansour.taleb@esprit.tn>
2025-10-29 20:56:13 +0100

    fix  USER,ADMIN

---
+++ src/main/java/com/alibou/keycloak/DemoController.java
@@ -0,0 +1,23 @@
+package com.alibou.keycloak;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/demo")
+public class DemoController {
+
+    @GetMapping
+    @PreAuthorize("hasRole('USER')")
+    public String hello() {
+        return "✅ Hello from Spring Boot & Keycloak - USER endpoint";
+    }
+
+    @GetMapping("/hello-2")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String hello2() {
+        return "🔒 Hello from Spring Boot & Keycloak - ADMIN endpoint";
+    }
+}
